78824a7acfc29335ec20a353af6c59e16963abee
mmogk <mmogk@my.bcit>
2023-01-24 10:57:55 -0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function About(){
+    return(
+        <main>
+            <>About Page
+            <br />
+            <Link href="/">Go Back</Link>
+            </>
+        </main>
+    )
+}
